8a1600ccfe224a23abe8515d7d17a3fff43e63f7
jordan <jharrison1@qa.com>
2021-06-30 10:50:54 +0100

    added example

---
+++ src/calc/App.java
@@ -0,0 +1,11 @@
+package calc;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println(Calculator.add(1, 2));
+		System.out.println(Calculator.add(11, 2));
+		System.out.println(Calculator.add(18, 2));
+	}
+
+}
---
+++ src/calc/Calculator.java
@@ -0,0 +1,9 @@
+package calc;
+
+public class Calculator {
+
+	public static int add(int a, int b) {
+		int sum = a + b;
+		return sum;
+	}
+}
